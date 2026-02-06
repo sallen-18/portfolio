50f849c997429c6bf50b832861f74f8ee941383d
sallen-18 <samgithub18@gmail.com>
2026-02-06 20:13:47 +0000

    Add astro adapter for AWS Amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    adapter: awsAmplify(),
+    output: 'server',
+});
