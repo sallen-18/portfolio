34f417e8d39e4eb6c0e4791a11b50e11535cac5f
sallen-18 <samgithub18@gmail.com>
2026-02-06 20:17:08 +0000

    remove amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
-    adapter: awsAmplify(),
-    output: 'server',
 });
